a22d613d4cd67eb4f462ce125f29f374f2edfb39
Ryan Oliveira <contato.ryanoliveira@outlook.com>
2022-03-25 21:24:56 -0300

    development: layout container

---
+++ src/components/Layouts/Container/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import Container from '@mui/material/Container'
+
+const LayoutContainer = props => <Container fixed>{ props.children }</Container>
+
+export default LayoutContainer
