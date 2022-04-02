32418253a47b999caf7f4565a22a38721eb2a8ee
Ryan Oliveira <contato.ryanoliveira@outlook.com>
2022-04-02 12:41:40 -0300

    feat: initial development

---
+++ src/views/Home/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import InitalSection from './InitialSection'
+
+import Header from '../../components/Modules/Header'
+import Container from '../../components/Layouts/Container'
+
+const Home = () =>
+  <>
+    <Header />
+
+    <br /><br /><br /><br /><br /><br />
+
+    <main>
+      <Container>
+        <InitalSection />
+      </Container>
+    </main>
+  </>
+
+export default Home
