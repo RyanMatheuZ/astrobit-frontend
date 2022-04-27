0950c76452fdd836ae93dac3533b8af9ff1a549e
EDER LEITE <ederaparecidoleite@hotmail.com>
2022-04-27 09:48:20 -0300

    feat: styles adjustments

--- src/views/Login/Index.jsx
+++ src/views/Login/Index.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import {Grid, Paper, Avatar, TextField, Stack, FormControlLabel, Checkbox, Button, Typography, Link} from '@mui/material';
+import {Grid, Box, Paper, Avatar, TextField, Stack, FormControlLabel, Checkbox, Button, Typography, Link} from '@mui/material';
 import LockIcon from '@mui/icons-material/Lock';
 import { useFormik } from 'formik';
 import * as yup from 'yup';
 
 const Login = () => {
-	const paperStyle={padding: 15, height:450, width:450, margin:"50px auto"}
+	const paperStyle={padding: 15, height: 'auto', width: 'auto', margin:"50px auto", fontFamily: 'Montserrat'}
 	const avatarStyle={backgroundColor:'#8979F2'}
 	const btnstyle={margin:'8px 0', backgroundColor:'#8979F2'}
+	const boxgrid={ width:400, fontFamily:'Montserrat', margin:'auto'}
 	const validationSchema = yup.object({
 		email: yup
 		  .string('Digite seu e-mail')
@@ -15,7 +16,7 @@ const Login = () => {
 		  .required('o e-mail é obrigatório'),
 		password: yup
 		  .string('Digite sua senha')
-		  .min(8, 'A senha deve ter no mínimo 8 caracteres')
+		  .min(5, 'A senha deve ter no mínimo 5 caracteres')
 		  .required('Senha é obrigatória'),
 	  });
 
@@ -31,25 +32,25 @@ const Login = () => {
 
 	return(
 
-		<Grid>
-			<Paper elevation={10} style={paperStyle} >
+		<Box style={boxgrid}>
+			<Paper elevation={10} style={paperStyle} align='center' >
 				<Grid align='center' >
 					<Avatar style={avatarStyle}><Stack><LockIcon/></Stack></Avatar>
 					<h2 margin="dense">Entrar</h2>
 				</Grid>
-				<form onSubmit={formik.handleSubmit} >
-				<TextField fullWidth id="email" name="email" label="Usuário" placeholder="Digite seu e-mail" value={formik.values.email} onChange={formik.handleChange} error={formik.touched.email && Boolean(formik.errors.email)} helperText={formik.touched.email && formik.errors.email} margin="dense"/>
-				<TextField fullWidth id="password" name="password" label="Senha" placeholder='Digite sua senha' type="password" value={formik.values.password} onChange={formik.handleChange} error={formik.touched.password && Boolean(formik.errors.password)} helperText={formik.touched.password && formik.errors.password} margin="dense"/>
-				<FormControlLabel
+				<form onSubmit={formik.handleSubmit}>
+				<TextField sx={{ width:'95%'}}  id="email" name="email" label="Usuário" placeholder="Digite seu e-mail" value={formik.values.email} onChange={formik.handleChange} error={formik.touched.email && Boolean(formik.errors.email)} helperText={formik.touched.email && formik.errors.email} margin="dense"/>
+				<TextField sx={{ width:'95%'}}  id="password" name="password" label="Senha" placeholder='Digite sua senha' type="password" value={formik.values.password} onChange={formik.handleChange} error={formik.touched.password && Boolean(formik.errors.password)} helperText={formik.touched.password && formik.errors.password} margin="dense"/>
+				<FormControlLabel sx={{ paddingLeft: 1, float:'left'}}
 				control={
 					<Checkbox name="checkedB" color="primary"/>
 					}
 					label="Lembra-me"
 				/>
-				<Button type='submit' color='primary' variant='contained' style={btnstyle} fullWidth >Sign In</Button>
+				<Button  type='submit' color='primary' variant='contained' style={btnstyle} sx={{ width:'95%'}} >Sign In</Button>
 				</form>
-				<Typography>
-					<Link href="#">
+				<Typography >
+					<Link href="#" >
 						Esqueceu a senha?
 					</Link>
 				</Typography>
@@ -60,7 +61,7 @@ const Login = () => {
 				</Typography>
 			</Paper>
 
-		</Grid>
+		</Box>
 	)
 }
 export default Login
