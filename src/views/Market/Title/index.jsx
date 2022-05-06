0f2a4220350b073128fa68aaf3b1e79710402ce0
Ryan Oliveira <contato.ryanoliveira@outlook.com>
2022-05-05 21:18:55 -0300

    feat: implementing coin filter

---
+++ src/views/Market/Title/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { Typography } from '@mui/material'
+
+import GreenSquare from '../../../components/Elements/GreenSquare'
+
+const Title = () =>
+  <Typography
+    component="h1"
+    sx={{ color: 'primaryColor', textAlign: 'center', fontSize: { xs: '30px', md: '38px' }, fontFamily: 'Montserrat', fontWeight: 700, mb: 4 }}
+  >
+    Acompanhe o mercado <GreenSquare />
+  </Typography>
+
+export default Title
