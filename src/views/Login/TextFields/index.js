b55c519c8b46de245a973361f58741435f08484d
Ryan Oliveira <contato.ryanoliveira@outlook.com>
2022-05-27 14:11:22 -0300

    refactor: change ext name

---
+++ src/views/Login/TextFields/index.js
@@ -0,0 +1,14 @@
+const textFields = [
+  {
+    name: 'login',
+    label: 'E-mail',
+    type: 'email',
+  },
+  {
+    name: 'password',
+    label: 'Senha',
+    type: 'password',
+  },
+];
+
+export default textFields;
---
+++ src/views/Register/TextFields/index.js
@@ -0,0 +1,21 @@
+const textFields = [
+  {
+    name: 'name',
+    label: 'Nome',
+    type: 'text',
+  },
+  {
+    name: 'login',
+    label: 'E-mail',
+    type: 'email',
+  },
+  {
+    name: 'password',
+    label: 'Senha',
+    type: 'password',
+  },
+];
+
+const investorProfile = ['Conservador', 'Moderado', 'Experiente'];
+
+export { textFields, investorProfile };
