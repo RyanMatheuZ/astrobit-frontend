89b064da387ae569dcf1d26103e3310ae6c830bd
Ryan Oliveira <contato.ryanoliveira@outlook.com>
2022-06-07 11:23:10 -0300

    refactor

--- src/components/Modules/Footer/index.jsx
+++ src/components/Modules/Footer/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { Box, Grid } from '@mui/material';
 import Logo from '../../Elements/Logo';
 import Container from '../../Layouts/Container';
